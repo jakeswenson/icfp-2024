bec50f9af3420e1454bfdf11585a0cfc2f186aae
teemobean <teemobean@gmail.com>
2024-06-29 14:46:43 -0700

    More data structures for 3D

--- solver/src/problems/spacetime.rs
+++ solver/src/problems/spacetime.rs
@@ -1,6 +1,7 @@
 use crate::problems::ProblemError;
 
 // Shared types
+#[derive(Copy, Clone)]
 enum Operator {
   ShiftLeft,
   ShiftRight,
@@ -16,11 +17,20 @@ enum Operator {
   NotEq,
 }
 
+#[derive(Copy, Clone)]
+enum OperatorArity {
+  Unary,
+  Binary,
+  Quad,
+}
+
+#[derive(Copy, Clone)]
 enum Parameter {
   A,
   B
 }
 
+#[derive(Copy, Clone)]
 enum CellValues {
   Op(Operator),
   Param(Parameter),
@@ -28,10 +38,31 @@ enum CellValues {
   EndState,
   Empty,
 }
+
+#[derive(Copy, Clone)]
 struct Cell {
   x: i32,
   y: i32,
   value: CellValues,
+}
+// End
+
+// Helpers
+fn OperatorArity(op : Operator) -> OperatorArity {
+  match op {
+    Operator::ShiftLeft => OperatorArity::Unary,
+    Operator::ShiftRight => OperatorArity::Unary,
+    Operator::ShiftUp => OperatorArity::Unary,
+    Operator::ShiftDown => OperatorArity::Unary,
+    Operator::Add => OperatorArity::Binary,
+    Operator::Sub => OperatorArity::Binary,
+    Operator::Mul => OperatorArity::Binary,
+    Operator::Div => OperatorArity::Binary,
+    Operator::Mod => OperatorArity::Binary,
+    Operator::Warp => OperatorArity::Quad,
+    Operator::Eq => OperatorArity::Binary,
+    Operator::NotEq => OperatorArity::Binary,
+  }
 }
 // End
 
@@ -43,7 +74,15 @@ struct Cell {
 
 // Evaluator
 fn evaluate(grid: Vec<Vec<Cell>>) {
-
+  let operators : Vec<_> = grid.iter().flatten().copied().filter(|cell| {
+    if let CellValues::Op(_) = cell.value {
+      true
+    } else {
+      false
+    }
+  }).collect();
+  for t in 1 .. 1000000 {
+  }
 }
 // End
 
