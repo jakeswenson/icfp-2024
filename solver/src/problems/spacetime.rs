524a850e4ceded468aaa7dd46fca9429e71e5365
teemobean <teemobean@gmail.com>
2024-06-29 13:42:00 -0700

    Add structures for 3D

--- solver/src/problems/spacetime.rs
+++ solver/src/problems/spacetime.rs
@@ -1,5 +1,55 @@
 use crate::problems::ProblemError;
 
+// Shared types
+enum Operator {
+  ShiftLeft,
+  ShiftRight,
+  ShiftUp,
+  ShiftDown,
+  Add,
+  Sub,
+  Mul,
+  Div,
+  Mod,
+  Warp,
+  Eq,
+  NotEq,
+}
+
+enum Parameter {
+  A,
+  B
+}
+
+enum CellValues {
+  Op(Operator),
+  Param(Parameter),
+  Val(i32),
+  EndState,
+  Empty,
+}
+struct Cell {
+  x: i32,
+  y: i32,
+  value: CellValues,
+}
+// End
+
+// Read from file
+// End
+
+// Compiler
+// End
+
+// Evaluator
+fn evaluate(grid: Vec<Vec<Cell>>) {
+
+}
+// End
+
+// Visualizer
+// End
+
 pub fn solve(
   _id: usize,
   input: String,
